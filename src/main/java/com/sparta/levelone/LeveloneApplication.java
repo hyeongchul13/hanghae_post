fb1685a12663d289d5c803f781619ee2ac356bde
hyeongchul13 <jc_22@naver.com>
2023-08-28 14:06:50 +0900

    init commit

---
+++ src/main/java/com/sparta/levelone/LeveloneApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.levelone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeveloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeveloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/levelone/LeveloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.levelone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeveloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
